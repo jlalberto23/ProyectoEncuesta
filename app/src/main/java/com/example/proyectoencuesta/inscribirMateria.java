facfd5193e96f49b84acb634ab76a2353e35398c
jlalberto23 <aa17025@ues.edu.sv>
2023-05-19 23:49:26 -0600

    Initial commit

---
+++ app/src/main/java/com/example/proyectoencuesta/inscribirMateria.java
@@ -0,0 +1,7 @@
+package com.example.proyectoencuesta;
+
+public class inscribirMateria {
+
+    //Esta pantalla solo debe mostrarse para los alumnos
+
+}
---
+++ app/src/main/java/com/example/proyectoencuesta/preguntasOrdenamiento.java
@@ -0,0 +1,4 @@
+package com.example.proyectoencuesta;
+
+public class preguntasOrdenamiento {
+}
