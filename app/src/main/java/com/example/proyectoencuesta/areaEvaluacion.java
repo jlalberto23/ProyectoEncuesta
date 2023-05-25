978c6f76f69dc68c8f10aafad68ac35e74d67f85
jlalberto23 <aa17025@ues.edu.sv>
2023-05-24 22:04:36 -0600

    getter y setter area evaluacion

--- app/src/main/java/com/example/proyectoencuesta/areaEvaluacion.java
+++ app/src/main/java/com/example/proyectoencuesta/areaEvaluacion.java
@@ -1,4 +1,40 @@
 package com.example.proyectoencuesta;
 
 public class areaEvaluacion {
+
+    private int idAreaEvaluativa, ordenNumerico;
+
+    private String nombreArea;
+
+    public areaEvaluacion(int idAreaEvaluativa,int ordenNumerico, String nombreArea){
+
+        this.idAreaEvaluativa = idAreaEvaluativa;
+        this.ordenNumerico = ordenNumerico;
+        this.nombreArea = nombreArea;
+
+    }
+
+    public int getIdAreaEvaluativa() {
+        return idAreaEvaluativa;
+    }
+
+    public int getOrdenNumerico() {
+        return ordenNumerico;
+    }
+
+    public String getNombreArea() {
+        return nombreArea;
+    }
+
+    public void setIdAreaEvaluativa(int idAreaEvaluativa) {
+        this.idAreaEvaluativa = idAreaEvaluativa;
+    }
+
+    public void setOrdenNumerico(int ordenNumerico) {
+        this.ordenNumerico = ordenNumerico;
+    }
+
+    public void setNombreArea(String nombreArea) {
+        this.nombreArea = nombreArea;
+    }
 }
