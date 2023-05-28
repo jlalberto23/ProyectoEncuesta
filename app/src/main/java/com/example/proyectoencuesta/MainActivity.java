9966831d769326e001105406b0f25dc8659ebed4
jlalberto23 <aa17025@ues.edu.sv>
2023-05-28 15:47:33 -0600

    update db

--- app/src/main/java/com/example/proyectoencuesta/MainActivity.java
+++ app/src/main/java/com/example/proyectoencuesta/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.proyectoencuesta;
 
-
-
+import android.widget.Toast;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -10,6 +9,7 @@ import android.widget.EditText;
 public class MainActivity extends Activity {
     Button ingresar, cancelar;
     EditText usuario, contra;
+    conexionDB BDhelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +19,13 @@ public class MainActivity extends Activity {
         cancelar = findViewById(R.id.btnCancelar);
         usuario = findViewById(R.id.txtUsuario);
         contra = findViewById(R.id.txtContraseña);
+
+        BDhelper = new conexionDB(this);
+
+        BDhelper.abrir();
+        String tost=BDhelper.llenarDatos();
+        BDhelper.cerrar();
+        Toast.makeText(this, tost, Toast.LENGTH_SHORT).show();
     }
 
 
