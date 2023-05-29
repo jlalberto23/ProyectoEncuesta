a98111487bf0957f9e96545af212a5d8c0c5ba7e
heinrichsv <sa04005@ues.edu.sv>
2023-05-28 22:51:36 -0600

    Agrega insert para tabla opcion_respuesta y fix de creacion de tablas

--- app/src/main/java/com/example/proyectoencuesta/opcionRespuesta.java
+++ app/src/main/java/com/example/proyectoencuesta/opcionRespuesta.java
@@ -2,19 +2,37 @@ package com.example.proyectoencuesta;
 
 public class opcionRespuesta {
 
-    private int idOpcionRespuesta, idTipoRespuesta;
-    private boolean esCorrecta;
+    private int idOpcionRespuesta, idPregunta;
+    private boolean esLaCorrecta;
     private String textoRespuesta;
-
-    public opcionRespuesta(int idOpcionRespuesta, int idTipoRespuesta, boolean esCorrecta, String textoRespuesta) {
-        this.idOpcionRespuesta = idOpcionRespuesta;
-        this.idTipoRespuesta = idTipoRespuesta;
-        this.esCorrecta = esCorrecta;
-        this.textoRespuesta = textoRespuesta;
-    }
 
     public opcionRespuesta() {
     }
+
+    public int getIdPregunta() {
+        return idPregunta;
+    }
+
+    public void setIdPregunta(int idPregunta) {
+        this.idPregunta = idPregunta;
+    }
+
+    public boolean isEsLaCorrecta() {
+        return esLaCorrecta;
+    }
+
+    public void setEsLaCorrecta(boolean esLaCorrecta) {
+        this.esLaCorrecta = esLaCorrecta;
+    }
+
+    public opcionRespuesta(int idOpcionRespuesta, int idPregunta, boolean esLaCorrecta, String textoRespuesta) {
+        this.idOpcionRespuesta = idOpcionRespuesta;
+        this.idPregunta = idPregunta;
+        this.esLaCorrecta = esLaCorrecta;
+        this.textoRespuesta = textoRespuesta;
+    }
+
+
 
     public int getIdOpcionRespuesta() {
         return idOpcionRespuesta;
@@ -22,22 +40,6 @@ public class opcionRespuesta {
 
     public void setIdOpcionRespuesta(int idOpcionRespuesta) {
         this.idOpcionRespuesta = idOpcionRespuesta;
-    }
-
-    public int getIdTipoRespuesta() {
-        return idTipoRespuesta;
-    }
-
-    public void setIdTipoRespuesta(int idTipoRespuesta) {
-        this.idTipoRespuesta = idTipoRespuesta;
-    }
-
-    public boolean isEsCorrecta() {
-        return esCorrecta;
-    }
-
-    public void setEsCorrecta(boolean esCorrecta) {
-        this.esCorrecta = esCorrecta;
     }
 
     public String getTextoRespuesta() {
