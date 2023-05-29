ae24c994c8e2cea23092578a3715c27dec47d834
crom503 <105618320+crom503@users.noreply.github.com>
2023-05-29 07:32:27 -0600

    1

--- app/src/main/java/com/example/proyectoencuesta/crearEncuesta.java
+++ app/src/main/java/com/example/proyectoencuesta/crearEncuesta.java
@@ -1,9 +1,11 @@
 package com.example.proyectoencuesta;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class crearEncuesta extends Activity {
+    Button guardar, cancelar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
