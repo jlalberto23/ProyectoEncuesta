67a59ff9a60a819747d234e1cbef20c1fd70a00b
crom503 <105618320+crom503@users.noreply.github.com>
2023-05-31 23:36:10 -0600

    cort

--- app/src/main/java/com/example/proyectoencuesta/agregarPreguntaResCorta.java
+++ app/src/main/java/com/example/proyectoencuesta/agregarPreguntaResCorta.java
@@ -2,11 +2,29 @@ package com.example.proyectoencuesta;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class agregarPreguntaResCorta  extends Activity {
+
+    conexionDB cn;
+    int numP;
+    String nom;
+    TextView nombre;
+    EditText pregunta;
+    Button next;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.agregar_pregunta_res_corta);
+        cn = new conexionDB(this);
+        Bundle extra = getIntent().getExtras();
+        numP = Integer.valueOf(extra.getString("numP"));
+        nom = extra.getString("nomEn");
+        nombre = findViewById(R.id.nomCuestilbl);
+        pregunta = findViewById(R.id.resCortatxt);
+        nombre.setText(nom);
     }
 }
