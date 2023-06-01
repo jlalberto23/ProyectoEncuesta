6cd87986e55c21dd2b54eb5ff933a81f53602ef0
jlalberto23 <aa17025@ues.edu.sv>
2023-05-31 22:15:31 -0600

    formularios agregar pregunta

--- app/src/main/java/com/example/proyectoencuesta/agregarPreguntaResCorta.java
+++ app/src/main/java/com/example/proyectoencuesta/agregarPreguntaResCorta.java
@@ -1,4 +1,12 @@
 package com.example.proyectoencuesta;
 
-public class agregarPreguntaResCorta {
+import android.app.Activity;
+import android.os.Bundle;
+
+public class agregarPreguntaResCorta  extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.agregar_pregunta_res_corta);
+    }
 }
--- app/src/main/java/com/example/proyectoencuesta/agregarPreguntaVyF.java
+++ app/src/main/java/com/example/proyectoencuesta/agregarPreguntaVyF.java
@@ -1,4 +1,12 @@
 package com.example.proyectoencuesta;
 
-public class agregarPreguntaVyF {
+import android.app.Activity;
+import android.os.Bundle;
+
+public class agregarPreguntaVyF extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.agregar_preguntas_vyf);
+    }
 }
