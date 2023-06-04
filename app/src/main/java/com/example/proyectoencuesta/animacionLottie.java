e1ab664a4f34d3fbacf839ec1222961815cb3371
jlalberto23 <aa17025@ues.edu.sv>
2023-06-04 00:16:41 -0600

    Agregar libreria Lottie
    Agregar libreria lottie
    Agregar icono
    Agregar botones

---
+++ app/src/main/java/com/example/proyectoencuesta/animacionLottie.java
@@ -0,0 +1,38 @@
+package com.example.proyectoencuesta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class animacionLottie extends AppCompatActivity {
+
+    TextView appname;
+    LottieAnimationView lottie;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animacion_lottie);
+
+        appname = findViewById(R.id.appname);
+        lottie = findViewById(R.id.lottie);
+
+        appname.animate().translationY(-1400).setDuration(2700).setStartDelay(0);
+        lottie.animate().translationX(2000).setDuration(2000).setStartDelay(2900);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(),pantallaBienvenida.class);
+                startActivity(i);
+
+            }
+        },5000);
+    }
+}
