c9b0fc5e36c7242bd02b56e1efe47c6eb99c54c4
jlalberto23 <aa17025@ues.edu.sv>
2023-06-11 00:05:32 -0600

    Permisos popUp

--- app/src/main/java/com/example/proyectoencuesta/pantallaBienvenida.java
+++ app/src/main/java/com/example/proyectoencuesta/pantallaBienvenida.java
@@ -4,15 +4,22 @@ import static android.view.View.INVISIBLE;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.Manifest;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class pantallaBienvenida extends Activity {
     Button ingresar, crear, sincuenta, llenarDBBtn;
     conexionDB cn;
+    int REQUEST_CODE = 200;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +39,20 @@ public class pantallaBienvenida extends Activity {
         crear.setOnClickListener(onclick);
         sincuenta.setOnClickListener(onclick);
         llenarDBBtn.setOnClickListener(onclick);
+        verificarPermisos();
+    }
+    private void verificarPermisos(){
+        int permisoAudio = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO);
+        int permisoWrite = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int permisoRead = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        if (permisoAudio == PackageManager.PERMISSION_GRANTED && permisoWrite == PackageManager.PERMISSION_GRANTED &&
+                permisoRead == PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(this,"Todos los permisos fueron concedidos correctamente",Toast.LENGTH_SHORT).show();
+        }else{
+            requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO,Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE},REQUEST_CODE);
+        }
     }
 
     View.OnClickListener onclick = new View.OnClickListener() {
